e86664ec1a391e0e56b70858b33f149cf4823e12
Liu ZH <yuan95338@gmail.com>
2021-08-07 22:10:55 +0800

    update distribution by pop,carpark and map

--- distribution/distribution_by_population.py
+++ distribution/distribution_by_population.py
@@ -8,6 +8,8 @@ with open("population_distribution.json") as f:
     population_distribution = json.load(f)
 
 for item in population_distribution:
+    item["latitude"] = float(item["latitude"])
+    item["longitude"] = float(item["longitude"])
     item["number_of_CS"] = round(float(item["population"].replace(',', '')) * EV_PER_CAPITA)
 
 with open('population_distribution.json', 'w') as f:
